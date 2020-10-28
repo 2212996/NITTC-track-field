de70d0686ea4ce2bfde7ef61d70d586e486b582f
2212996 <51160826+2212996@users.noreply.github.com>
2020-10-29 01:44:00 +0900

    h

--- things/home/home.js
+++ things/home/home.js
@@ -8,7 +8,7 @@ $(function() {
   });
   
   $(".top_pic").bgSwitcher({
-    images: ["back1.jpg","back2.JPG","back3.jpg"], 
+    images: ["../assets/back1.jpg","../assets/back2.JPG","../assets/back3.jpg"], 
     interval:6000,
     loop:true,
     duration:1500,
@@ -146,6 +146,19 @@ $(function() {
   
   $(window).scroll(function (){
     $(".content_pic5").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight / 5){
+        $(this).addClass("fade_on");
+      } else {
+        $(this).removeClass("fade_on");
+      }
+    });
+  });
+  
+  $(window).scroll(function (){
+    $(".content_pic6").each(function(){
       var imgPos = $(this).offset().top;
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
